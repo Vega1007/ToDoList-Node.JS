6980825034c9182d189a3cd90174cd4912aaeb10
AlexandreMiguel02 <167123067+Vega1007@users.noreply.github.com>
2025-10-30 17:23:25 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,1 @@
-const sql = require(`mysql2/promise`);
 
-const pool = sql.createPool({
-    host: `localhost`,
-    user: `root`,
-    password: `09843Ale`,
-    database: `todolist_app`
-})
-
-module.exports = pool
